96c1d419a7c39aada3fa986446cc8c08e7fddaad
Bethsyf <bethsyfalcon@gmail.com>
2022-03-23 22:04:32 -0500

    pintar productos perro

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,18 @@
-impor
+import { showCards } from "../modules/showCard.js";
+
+let contenedorPerros = document.getElementById('containerdog');
+let contenedorGatos = document.getElementById('containercat');
+let contenedorOtrasMascotas = document.getElementById('containerothers');
+
+document.addEventListener('DOMContentLoaded', async () => {
+    let res = await fetch('http://localhost:4000/products');
+    let data = await res.json();
+
+    let productosPerro = data.filter(item => item.category == 'dog');
+    let productosGato = data.filter(item => item.category == 'cat');
+    let productosOtrasMascotas = data.filter(item => item.category == 'other');
+
+    showCards(productosPerro, contenedorPerros);
+    showCards(productosGato, contenedorGatos);
+    showCards(productosOtrasMascotas, contenedorOtrasMascotas);
+})
